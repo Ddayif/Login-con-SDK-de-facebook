87be5b353a98298c5d6a8d98bf79d3a5972e5268
Joselin Grez <joselingrez.m@hotmail.es>
2017-07-05 12:31:05 -0300

    Agregando otro boton

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,11 @@ function checkLoginState() {
     statusChangeCallback(response);
   });
 }
+
+var finished_rendering = function() {
+console.log("finished rendering plugins");
+var spinner = document.getElementById("spinner");
+spinner.removeAttribute("style");
+spinner.removeChild(spinner.childNodes[0]);
+}
+FB.Event.subscribe('xfbml.render', finished_rendering);
